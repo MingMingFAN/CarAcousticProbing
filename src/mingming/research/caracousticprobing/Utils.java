592150e63bed7083bc5892c6436e0f1f4c2d8ad9
MingMingFAN <mfan@uncc.edu>
2014-09-28 11:24:40 -0400

    first commit

---
+++ src/mingming/research/caracousticprobing/Utils.java
@@ -0,0 +1,7 @@
+package mingming.research.caracousticprobing;
+
+public final class Utils {
+
+	public static int probing_duration = 1000;  //milliseconds
+	public static int repeat_period = 5000;
+}
